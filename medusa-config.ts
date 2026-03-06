b2440f33151943b84fe98575388b4c02973f2044
Niran Millet <nini@Nirans-MacBook-Pro.local>
2026-03-06 09:44:58 +0700

    payments implemented

--- medusa-config.ts
+++ medusa-config.ts
@@ -17,5 +17,40 @@ export default defineConfig({
     {
       resolve: "./src/modules/affiliate",
     },
+    {
+    resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+            {
+            resolve: "medusa-payment-yoco",
+            id: "yoco",
+            options: {
+              secretKey: process.env.YOCO_SECRET_KEY,
+              debug: true,
+              successUrl: process.env.YOCO_SUCCESS_URL,
+              cancelUrl: process.env.YOCO_CANCEL_URL,
+              failureUrl: process.env.YOCO_FAILURE_URL,
+            },
+          },
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+            },
+          },
+          {
+            resolve: "medusa-payment-dpopay",
+            id: "dpopay",
+            options: {
+              companyToken: "8D3DA73D-9D7F-4E09-96D4-3D44E7A83EA3",
+              serviceType: "5525"
+            }
+          }
+          
+        ],
+      },
+    },
+
   ],
 })
